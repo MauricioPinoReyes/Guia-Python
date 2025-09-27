0cb80c4d093dcd0863d33872bdcfcf7050fa8740
Mauricio Pino <mauricio.pino.log@gmail.com>
2025-09-26 22:35:39 -0300

    Avace preparacion prueba 1

--- 1-C2-B50-V/Prepaparacion Eva2/ejercicio_Eva2/main.py
+++ 1-C2-B50-V/Prepaparacion Eva2/ejercicio_Eva2/main.py
@@ -1,11 +1,33 @@
 
+from estudiante import Estudiante
 from nota import Nota
 from rut import Rut
 from carrera import Carrera
 from fecha import Fecha
 
+def agregarEstudiante():
+    listaEstudiantes = [] 
+    while True:
+        rutEstudiante = int(input("Ingrese rut del estudiante: "))
+        dv = input("Ingrese digito verificador del estudiante: ")
+        rut = Rut(rutEstudiante, dv)
+        nombre = input("Ingrese nombre del estudiante: ")
+        carreraEstudiante = input("Ingrese carrera del estudiante: ")
+        carrera = Carrera(carreraEstudiante)
+        estudiante = Estudiante(rut, nombre, carrera)
+        listaEstudiantes.append(estudiante)
+        
+        salida = input("Desea ingresar otro estudiante? S/N: ")
+        if salida.upper() == "N":
+            break
+    
+    return listaEstudiantes  
 
-listaNotas=[]
+
+estudiantes = agregarEstudiante()
+print(estudiantes)
+
+""" listaNotas=[]
 rut=Rut(12345678,"9")
 carrera=Carrera("Ingeniería Informática")
 fecha=Fecha(20,10,2021)
@@ -17,5 +39,5 @@ no=Nota(rut,"Jose Luis",carrera,11,5.2,"Inglés",2,fecha)
 listaNotas.append(no)
 
 for x in listaNotas:
-    print(x.mostrarNota())
+    print(x.mostrarNota()) """
     
